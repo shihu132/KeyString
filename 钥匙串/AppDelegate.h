312796e4ac6b2b8de281564730543d0eb4398443
shihu <13261089216@163.com>
2018-12-10 14:23:26 +0800

    Initial Commit

---
+++ 钥匙串/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  钥匙串
+//
+//  Created by joyshow on 2018/12/10.
+//  Copyright © 2018年 石虎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
