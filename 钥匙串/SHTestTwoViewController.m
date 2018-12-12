163a58dd1fe66c031430b0978130d256c21cf4dd
shihu <13261089216@163.com>
2018-12-12 14:33:58 +0800

    fork  钥匙串 在 twoViewController add 生命周期

--- 钥匙串/SHTestTwoViewController.m
+++ 钥匙串/SHTestTwoViewController.m
@@ -19,14 +19,13 @@
     // Do any additional setup after loading the view.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+-(void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    NSLog(@"111111111111111111111111111");
 }
-*/
+
+- (void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:animated];
+}
 
 @end
