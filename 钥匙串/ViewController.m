61a8fd46254aad718ec0de8dec663f096cb7fe8f
shihu <13261089216@163.com>
2018-12-12 10:29:22 +0800

    钥匙串初始化

--- 钥匙串/ViewController.m
+++ 钥匙串/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#import "KeychainItemWrapper.h"
 @interface ViewController ()
 
 @end
@@ -18,6 +18,34 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
+#pragma mark - keychain读取
+- (IBAction)keychainRead:(id)sender {
+    
+    KeychainItemWrapper *wrapper = [[KeychainItemWrapper alloc]initWithIdentifier:@"Username"accessGroup:nil];
+    NSLog(@"pwd = %@,account = %@",[wrapper objectForKey:(__bridge id)kSecValueData],[wrapper objectForKey:(__bridge id)kSecAttrAccount]);
+    
+}
+#pragma mark - keychain存储
+- (IBAction)clickJieM:(id)sender {
+    
+    KeychainItemWrapper *wrapper = [[KeychainItemWrapper alloc]initWithIdentifier:@"Username"accessGroup:nil];
+    //保存帐号
+    [wrapper setObject:@"张三" forKey:(__bridge id)kSecAttrAccount];
+    //保存密码
+    [wrapper setObject:@"123456" forKey:(__bridge id)kSecValueData];
+}
 
+- (IBAction)clickJieM2:(id)sender {
+    
+    KeychainItemWrapper *wrapper = [[KeychainItemWrapper alloc]initWithIdentifier:@"Username"accessGroup:nil];
+    //保存帐号
+    [wrapper setObject:@"李四" forKey:(__bridge id)kSecAttrAccount];
+    //保存密码
+    [wrapper setObject:@"654321" forKey:(__bridge id)kSecValueData];
+}
 
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    
+    [self becomeFirstResponder];
+}
 @end
